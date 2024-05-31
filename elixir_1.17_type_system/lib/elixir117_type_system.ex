235541432123d6d0465d3b447f0d0ad2276b83ba
Hentioe <meow.i5.br@gmail.com>
2024-06-01 01:29:04 +0800

    Add elixir_1.17_type_system

---
+++ elixir_1.17_type_system/lib/elixir117_type_system.ex
@@ -0,0 +1,18 @@
+defmodule Elixir117TypeSystem do
+  @moduledoc """
+  Documentation for `Elixir117TypeSystem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixir117TypeSystem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir_1.17_type_system/test/elixir117_type_system_test.exs
@@ -0,0 +1,8 @@
+defmodule Elixir117TypeSystemTest do
+  use ExUnit.Case
+  doctest Elixir117TypeSystem
+
+  test "greets the world" do
+    assert Elixir117TypeSystem.hello() == :world
+  end
+end
